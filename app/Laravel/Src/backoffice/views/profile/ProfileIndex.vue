abe5ec19fd4da762a70a20a6cf2e145f1ab55831
Joseph Dela Cruz Jr <josephdelacruzjr4@gmail.com>
2025-05-31 00:06:10 +0800

    feat: enhance profile form handling by updating submission logic and improving validation; streamline avatar upload process

--- app/Laravel/Src/backoffice/views/profile/ProfileIndex.vue
+++ app/Laravel/Src/backoffice/views/profile/ProfileIndex.vue
@@ -3,7 +3,7 @@
         <div class="container-xxl flex-grow-1 container-p-y">
             <Notification />
             <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Profile /</span> Account</h4>
-            <form>
+            <form @submit.prevent="handleSubmit">
                 <div class="card mb-4">
                     <h5 class="card-header">Profile Details</h5>
                     <div class="card-body">
@@ -25,56 +25,54 @@
                     </div>
                     <hr class="my-0">
                     <div class="card-body">
-                        <form @submit.prevent="handleSubmit">
-                            <div class="row">
-                                <div class="mb-3 col-md-6">
-                                    <label for="input_firstname" class="form-label">First Name</label>
-                                    <input class="form-control" type="text" v-model="profile.firstname" id="input_firstname" placeholder="firstname">
-                                    <small v-if="errorStore.validations.errors?.firstname" class="text-danger">
-                                        {{ errorStore.validations.errors.firstname[0] }}
-                                    </small>
-                                </div>
-                                <div class="mb-3 col-md-6">
-                                    <label for="input_lastname" class="form-label">Last Name</label>
-                                    <input class="form-control" type="text" v-model="profile.lastname" id="input_lastname" placeholder="lastname">
-                                    <small v-if="errorStore.validations.errors?.lastname" class="text-danger">
-                                        {{ errorStore.validations.errors.lastname[0] }}
-                                    </small>
-                                </div>
-                                <div class="mb-3 col-md-6">
-                                    <label for="input_middlename" class="form-label">Middlename</label>
-                                    <input class="form-control" type="text" v-model="profile.middlename" id="input_middlename" placeholder="middlename">
-                                    <small v-if="errorStore.validations.errors?.middlename" class="text-danger">
-                                        {{ errorStore.validations.errors.middlename[0] }}
-                                    </small>
-                                </div>
-                                <div class="mb-3 col-md-6">
-                                    <label for="input_suffix" class="form-label">Suffix</label>
-                                    <input class="form-control" type="text" v-model="profile.suffix" id="input_suffix" placeholder="suffix">
-                                    <small v-if="errorStore.validations.errors?.suffix" class="text-danger">
-                                        {{ errorStore.validations.errors.suffix[0] }}
-                                    </small>
-                                </div>
-                                <div class="mb-3 col-md-6">
-                                    <label for="input_username" class="form-label">Username</label>
-                                    <input class="form-control" type="text" v-model="profile.username" id="input_username" placeholder="username">
-                                    <small v-if="errorStore.validations.errors?.username" class="text-danger">
-                                        {{ errorStore.validations.errors.username[0] }}
-                                    </small>
-                                </div>
-                                <div class="mb-3 col-md-6">
-                                    <label for="input_email" class="form-label">Email</label>
-                                    <input class="form-control" type="text" v-model="profile.email" id="input_email" placeholder="email">
-                                    <small v-if="errorStore.validations.errors?.email" class="text-danger">
-                                        {{ errorStore.validations.errors.email[0] }}
-                                    </small>
-                                </div>
+                        <div class="row">
+                            <div class="mb-3 col-md-6">
+                                <label for="input_firstname" class="form-label">First Name</label>
+                                <input class="form-control" type="text" v-model="profile.firstname" id="input_firstname" placeholder="firstname">
+                                <small v-if="errorStore.validations.errors?.firstname" class="text-danger">
+                                    {{ errorStore.validations.errors.firstname[0] }}
+                                </small>
                             </div>
-                            <div class="demo-inline-spacing d-flex justify-content-end">
-                                <RouterLink :to="{ name: 'Index' }" class="btn btn-outline-secondary" @click="errorStore.validations = {}">Cancel</RouterLink>
-                                <button type="submit" class="btn btn-primary me-2">{{ profileStore.isLoading ? 'Loading..' : 'Save changes' }}</button>
+                            <div class="mb-3 col-md-6">
+                                <label for="input_lastname" class="form-label">Last Name</label>
+                                <input class="form-control" type="text" v-model="profile.lastname" id="input_lastname" placeholder="lastname">
+                                <small v-if="errorStore.validations.errors?.lastname" class="text-danger">
+                                    {{ errorStore.validations.errors.lastname[0] }}
+                                </small>
                             </div>
-                        </form>
+                            <div class="mb-3 col-md-6">
+                                <label for="input_middlename" class="form-label">Middlename</label>
+                                <input class="form-control" type="text" v-model="profile.middlename" id="input_middlename" placeholder="middlename">
+                                <small v-if="errorStore.validations.errors?.middlename" class="text-danger">
+                                    {{ errorStore.validations.errors.middlename[0] }}
+                                </small>
+                            </div>
+                            <div class="mb-3 col-md-6">
+                                <label for="input_suffix" class="form-label">Suffix</label>
+                                <input class="form-control" type="text" v-model="profile.suffix" id="input_suffix" placeholder="suffix">
+                                <small v-if="errorStore.validations.errors?.suffix" class="text-danger">
+                                    {{ errorStore.validations.errors.suffix[0] }}
+                                </small>
+                            </div>
+                            <div class="mb-3 col-md-6">
+                                <label for="input_username" class="form-label">Username</label>
+                                <input class="form-control" type="text" v-model="profile.username" id="input_username" placeholder="username">
+                                <small v-if="errorStore.validations.errors?.username" class="text-danger">
+                                    {{ errorStore.validations.errors.username[0] }}
+                                </small>
+                            </div>
+                            <div class="mb-3 col-md-6">
+                                <label for="input_email" class="form-label">Email</label>
+                                <input class="form-control" type="text" v-model="profile.email" id="input_email" placeholder="email">
+                                <small v-if="errorStore.validations.errors?.email" class="text-danger">
+                                    {{ errorStore.validations.errors.email[0] }}
+                                </small>
+                            </div>
+                        </div>
+                        <div class="demo-inline-spacing d-flex justify-content-end">
+                            <RouterLink :to="{ name: 'Index' }" class="btn btn-outline-secondary" @click="errorStore.validations = {}">Cancel</RouterLink>
+                            <button type="submit" class="btn btn-primary me-2">{{ profileStore.isLoading ? 'Loading..' : 'Save changes' }}</button>
+                        </div>
                     </div>
                 </div>
             </form>
@@ -121,19 +119,35 @@
         profileStore.profile = {};
     });
 
-    const handleSubmit = async () => {
-        await profileStore.profileUpdate(profile.value, router);
-    };
-
     const fileInput = ref(null);
+    const selectedFile = ref(null);
     const avatarUrl = ref(defaultAvatar);
     const onFileChange = ({ target: { files } }) => {
         if (files?.[0]) {
+            selectedFile.value = files[0];
             avatarUrl.value = URL.createObjectURL(files[0]);
         }
     };
     const resetImage = () => {
         fileInput.value.value = '';
         avatarUrl.value = defaultAvatar;
+        selectedFile.value = null;
+    };
+
+    const handleSubmit = async () => {
+        const formData = new FormData();
+
+        formData.append('firstname', profile.value.firstname);
+        formData.append('lastname', profile.value.lastname);
+        formData.append('middlename', profile.value.middlename);
+        formData.append('suffix', profile.value.suffix);
+        formData.append('username', profile.value.username);
+        formData.append('email', profile.value.email);
+
+        if (selectedFile.value) {
+            formData.append('image', selectedFile.value);
+        }
+
+        await profileStore.profileUpdate(formData, router);
     };
 </script>
