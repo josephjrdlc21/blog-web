ab7dc9becd568a1364dd4317df29bf807ec4b5d6
Joseph Dela Cruz Jr <josephdelacruzjr4@gmail.com>
2025-06-01 15:10:27 +0800

    feat: remove CKEditor integration from main.js

--- app/Laravel/Src/backoffice/main.js
+++ app/Laravel/Src/backoffice/main.js
@@ -1,7 +1,6 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import Persistedstate from 'pinia-plugin-persistedstate';
-import { Ckeditor } from '@ckeditor/ckeditor5-vue';
 
 import App from './App.vue';
 import router from './router';
@@ -13,5 +12,4 @@ pinia.use(Persistedstate);
 
 app.use(pinia);
 app.use(router);
-app.use(Ckeditor);
 app.mount('#app');
